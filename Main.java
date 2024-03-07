43c4d224ee6817f7b2b2cb6d3fdd49f029436b0e
saikrishna <gadadas.saikrishna@gmail.com>
2024-03-07 10:12:53 +0530

    first code

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+[D[D[D[B[B[B[A[A[A[A[C[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[main	
+
+
+
+
